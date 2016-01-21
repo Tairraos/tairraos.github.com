bce502f9198b2d43bd9a8760f2e8d38c53c10357
Xiaole Tao <xiaolet@cisco.com>
2016-01-21 16:24:10 +0800

    update

--- article/f2e-interview.md
+++ article/f2e-interview.md
@@ -1,68 +1,157 @@
+# 前端工程师面试问卷
 
-使用方法如下:
+贡献者：Xiaole Tao (xiaole.tao@gmail.com)
 
-广度评估：
+最后更新：2016.1.20
+
+
+
+### 目录
+
+[1 Web规范，HTTP协议，浏览器](1_Web规范，HTTP协议，浏览器)
+
+2 HTML和CSS
+
+3 JavaScript
+
+4 开发
+
+5 常见坑
+
+6 相关技能
+
+7 全栈工程师
+
+附：使用方法
+
+
+
+
+
+## 1 Web规范，HTTP协议，浏览器
+
+#### 语义化
+
+
+
+#### doctype
+
+废弃tag和属性
+
+内核差异，兼容处理，移动端，响应式
+
+Css reset，Css hack, i18n/l10n, MIME
+
+HTTP AUTH, HTTP安全，跨域访问，JSONP，协议限制(http不能嵌套https内容)
+
+常见HTTP响应含义，200, 302, 403, 404, 500, 501, 502
+
+## 2 HTML和CSS
+
+Audio，Video，Canvas，WebGL，file api，device api
+
+Web worker，Websocket，WebStorage，indexedDb
+
+css media, viewport，touch事件，drag drop
+
+严格模式，类型数组（Float32Array Float64Array Int8Array Int16Array)
+
+布局，盒模型，选择器优先级，增强的伪类，css sprite
+
+web font，transition，3d
+
+## 3 JavaScript
+
+数据类型，继承，闭包，原型链，JSON，正则
+
+作用域，模块化，自定义事件
+
+事件机制，异步回调，lazy load，模板引擎
+
+MVC，前后端分离，Nodejs，Minify，常用框架和工具
+
+内存泄漏，线程，安全，XML解析
+
+## 5 开发
+
+环境搭建，单元测试，安全测试，性能测试, 压力测试, 性能调优
+
+Minify，调试方法（worker，source map)
+
+Scrum协作，版本管理，SSH KEY
+
+## 4 常见坑
+
+PNG不能显示 - IE PNG补丁
+
+浏览器渲染不一致 - CSS RESRT
+
+HTTP调用HTTPS
+
+IFRAME的SRC并非同一域 - SRC写成about:blank，然后用ajax get把页面加载进去
+
+AJAX的URL不是同一个域 - JSONP
+
+## 6 相关技能
+
+画图切图，制表，压缩图片
+
+TypeScript，CoffeeScript，less/sass
+
+流行的JS工具库，和开发框架
+
+HTTP(s) Request/Response，Load balance
+
+服务器端语言，J2EE，PHP，python, ruby，perl, .net
+
+## 7 全栈工程师
+
+TCP/IP，Socket，主要的RFC
+
+Video，Audio，WEBGL
+
+系统架构，数据库，TA，UED
+
+流程图，UML，思维导图
+
+Native开发，Mobile开发，主流编程语言
+
+UNIX/Linux，WIN，MAC
+
+Mail system, FTP，SSH，WebServer，DB，TOMCAT
+
+SEO，英语
+
+
+
+
+
+## 附：使用方法
+
+面试目标：本面试问卷用于测试职位候选人的前端知识深度及广度。
+
+##### 广度评估：
+
 下面有6个方面如果每个方面有大量的B或者B以上, 少量的C或C以上, 加1分。
+
 如果大量的B没有C, 加0.5分
+
 4分为合格，5分为优秀，全栈为附加分，满分6分。
+
 深度评估
+
 下面如果JavaScript和开发方面，单项评估为E，加1分。
+
 如果是D则0.5分。
+
 1分为合格，2分为优秀，全栈为附加分，满分3分
 
 A - 不知道 / 理解错误
+
 B - 听说过 / 理解正确
+
 C - 感兴趣 / 初步了解
+
 D - 使用过 / 深刻理解
+
 E - 善长 / 深入研究
-
-1 Web规范，HTTP协议，浏览器
-语义化，doctype，废弃tag和属性
-内核差异，兼容处理，移动端，响应式
-Css reset，Css hack, i18n/l10n, MIME
-HTTP AUTH, HTTP安全，跨域访问，JSONP，协议限制(http不能嵌套https内容)
-常见HTTP响应含义，200, 302, 403, 404, 500, 501, 502
-
-2 HTML和CSS
-Audio，Video，Canvas，WebGL，file api，device api
-Web worker，Websocket，WebStorage，indexedDb
-css media, viewport，touch事件，drag drop
-严格模式，类型数组（Float32Array Float64Array Int8Array Int16Array)
-布局，盒模型，选择器优先级，增强的伪类，css sprite
-web font，transition，3d
-
-3 JavaScript：   
-数据类型，继承，闭包，原型链，JSON，正则
-作用域，模块化，自定义事件
-事件机制，异步回调，lazy load，模板引擎
-MVC，前后端分离，Nodejs，Minify，常用框架和工具
-内存泄漏，线程，安全，XML解析
-
-4 常见坑
-PNG不能显示 - IE PNG补丁
-浏览器渲染不一致 - CSS RESRT
-HTTP调用HTTPS
-IFRAME的SRC并非同一域 - SRC写成about:blank，然后用ajax get把页面加载进去
-AJAX的URL不是同一个域 - JSONP
-
-4 开发
-环境搭建，单元测试，安全测试，性能测试, 压力测试, 性能调优
-Minify，调试方法（worker，source map)
-Scrum协作，版本管理，SSH KEY
-
-5 相关技能
-画图切图，制表，压缩图片
-TypeScript，CoffeeScript，less/sass
-流行的JS工具库，和开发框架
-HTTP(s) Request/Response，Load balance
-服务器端语言，J2EE，PHP，python, ruby，perl
-
-6 全栈工程师
-TCP/IP，Socket，主要的RFC
-Video，Audio，WEBGL
-系统架构，数据库，TA，UED
-流程图，UML，思维导图
-Native开发，Mobile开发，主流编程语言
-UNIX/Linux，WIN，MAC
-Mail system, FTP，SSH，WebServer，DB，TOMCAT
-SEO，英语
--- bookmarkTools/utTool.js
+++ bookmarkTools/utTool.js
@@ -3,205 +3,208 @@
  * javascript:(function(){var tool='utTool',css='utTool',p=window.pageTools;if(p&&p[tool]){p[tool].run();return;}var d=document,s=d.createElement('script');s.setAttribute('src','//localhost/tools/loader.js?tool='+tool+'&css='+css);d.body.appendChild(s);}());
  */
 (function ($) {
-	'use strict';
-	var toolName = 'utTool', win = window,
-		tools = win.pageTools = win.pageTools || {},
-		conf = tools.conf = tools.conf || {};
+    'use strict';
+    var toolName = 'utTool', win = window,
+        tools = win.pageTools = win.pageTools || {},
+        conf = tools.conf = tools.conf || {};
 
-	conf = {
-		baseURL: location.protocol + '//' + location.host + location.pathname.replace(/UT-Parallel\/.*/, 'UT-Parallel/'),
-		mode: '显示全部',
-		filePool: [],
-		dataPool: {},
-		sourceData: {},
-		targetData: {},
-		sourceVersion: 0,
-		targetVersion: 0,
-		$form: null, //查询表单
-		$span: null,
-		$sourceVer: null,
-		$targetVer: null,
-		$button: null,
-		$meta: null,
-		$mode: null,
-		$table: null
-	};
+    conf = {
+        baseURL: location.protocol + '//' + location.host + location.pathname.replace(/UT-Parallel\/.*/, 'UT-Parallel/'),
+        mode: '显示全部',
+        filePool: [],
+        dataPool: {},
+        sourceData: {},
+        targetData: {},
+        sourceVersion: 0,
+        targetVersion: 0,
+        $form: null, //查询表单
+        $span: null,
+        $sourceVer: null,
+        $targetVer: null,
+        $button: null,
+        $meta: null,
+        $mode: null,
+        $table: null
+    };
 
-	tools[toolName] = $.extend({}, tools.base, {
-		run: function () {
-			showForm();
-		}
-	});
+    tools[toolName] = $.extend({}, tools.base, {
+        run: function () {
+            showForm();
+        }
+    });
 
-	tools[toolName].run();
+    tools[toolName].run();
 
-	/**
-	 * 取到最后两个build版本
-	 */
-	function getVersion() {
-		var versionHistory = getPathContent(conf.baseURL, '#buildHistory .build-row:not(.transitive)');
-		conf.sourceVersion = +versionHistory.eq(0).find('td').eq(0).text().replace(/[^\d]/g, '');
-		conf.targetVersion = +versionHistory.eq(1).find('td').eq(0).text().replace(/[^\d]/g, '');
-	}
+    /**
+     * 取到最后两个build版本
+     */
+    function getVersion() {
+        var versionHistory = getPathContent(conf.baseURL, '#buildHistory .build-row:not(.transitive)');
+        conf.sourceVersion = +versionHistory.eq(0).find('td').eq(0).text().replace(/[^\d]/g, '');
+        conf.targetVersion = +versionHistory.eq(1).find('td').eq(0).text().replace(/[^\d]/g, '');
+    }
 
-	/**
-	 * 显示表单
-	 */
-	function showForm() {
-		getVersion();
-		conf.$form = $('<div id="coverageForm"></div>').prependTo('body').on('keydown', function (e) {
-			if (e.keyCode === 13) {
-				conf.$button.click();
-			} else if (e.keyCode === 27) {
-				conf.$form.remove();
-			}
-		});
-		conf.$span = $('<span>您需要在哪两个版本间生成对比报告?</span>').appendTo(conf.$form);
-		conf.$sourceVer = $('<input id="sourceVer" />').appendTo(conf.$form).val(conf.sourceVersion).focus();
-		conf.$targetVer = $('<input id="targetVer" />').appendTo(conf.$form).val(conf.targetVersion);
-		conf.$button = $('<button>开始生成</button>').on('click', startAnalytic).appendTo(conf.$form);
-		conf.$meta = $('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />').appendTo('head'); //确定中文能正确显示
-	}
+    /**
+     * 显示表单
+     */
+    function showForm() {
+        getVersion();
+        conf.$form = $('<div id="coverageForm"></div>').prependTo('body').on('keydown', function (e) {
+            if (e.keyCode === 13) {
+                conf.$button.click();
+            } else if (e.keyCode === 27) {
+                conf.$form.remove();
+            }
+        });
+        conf.$span = $('<span>您需要在哪两个版本间生成对比报告?</span>').appendTo(conf.$form);
+        conf.$sourceVer = $('<input id="sourceVer" />').appendTo(conf.$form).val(conf.sourceVersion).focus();
+        conf.$targetVer = $('<input id="targetVer" />').appendTo(conf.$form).val(conf.targetVersion);
+        conf.$button = $('<button>开始生成</button>').on('click', startAnalytic).appendTo(conf.$form);
+        conf.$meta = $('<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />').appendTo('head'); //确定中文能正确显示
+    }
 
-	/**
-	 * 分析开始
-	 * @returns {*}
-	 */
-	function startAnalytic() {
-		if (!checkNumber()) {
-			return conf.$span.text('版本号必须为数字').css('color', 'red');
-		}
-		if (conf.$sourceVer.val() > conf.$targetVer.val()) {
-			var v = conf.$targetVer.val();
-			conf.$targetVer.val(conf.$sourceVer.val());
-			conf.$sourceVer.val(v);
-		}
-		analyticURL(conf.baseURL + conf.$sourceVer.val() + '/cobertura/', conf.sourceData);
-		analyticURL(conf.baseURL + conf.$targetVer.val() + '/cobertura/', conf.targetData);
-		$.each($.extend({}, conf.sourceData, conf.targetData), function (name) {
-			return ($.inArray(name, conf.filePool) === -1) && conf.filePool.push(name);
-		});
-		return genReport(conf.filePool.sort()) && showReport();
-	}
+    /**
+     * 分析开始
+     * @returns {*}
+     */
+    function startAnalytic() {
+        if (!checkNumber()) {
+            return conf.$span.text('版本号必须为数字').css('color', 'red');
+        }
+        if (conf.$sourceVer.val() > conf.$targetVer.val()) {
+            var v = conf.$targetVer.val();
+            conf.$targetVer.val(conf.$sourceVer.val());
+            conf.$sourceVer.val(v);
+        }
+        analyticURL(conf.baseURL + conf.$sourceVer.val() + '/cobertura/', conf.sourceData);
+        analyticURL(conf.baseURL + conf.$targetVer.val() + '/cobertura/', conf.targetData);
+        $.each($.extend({}, conf.sourceData, conf.targetData), function (name) {
+            return ($.inArray(name, conf.filePool) === -1) && conf.filePool.push(name);
+        });
+        return genReport(conf.filePool.sort()) && showReport();
+    }
 
-	/**
-	 * 生成报表
-	 * @param filePool
-	 * @returns {*}
-	 */
-	function genReport(filePool) {
-		var tempColor, today = new Date();
-		conf.title = document.title = 'Build ' + $('#sourceVer').val() + ' vs Build ' + $('#targetVer').val();
-		conf.$mode = $('<span class="change">' + conf.mode + '</span>');
-		conf.$table = $('<table id="report"><tr><td>文件路径</td><td>条件覆盖率</td><td>条件覆盖数</td><td>行覆盖率</td><td>行覆盖数</td></tr></table>');
-		conf.$table.prepend($('<tr></tr>').append($('<td colspan="5">' + conf.title + ', 报表生成时间：' + (today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + (today.getDate() + 1) + ' ' + today.getHours() + ':' + today.getMinutes()) + ' 报表方式：</td>').append(conf.$mode)));
+    /**
+     * 生成报表
+     * @param filePool
+     * @returns {*}
+     */
+    function genReport(filePool) {
+        var tempColor, today = new Date();
+        conf.title = document.title = 'Build ' + $('#targetVer').val() + ' vs Build ' + $('#sourceVer').val();
+        conf.$mode = $('<span class="change">' + conf.mode + '</span>');
+        conf.$table = $('<table id="report"><tr><td>文件路径</td><td>条件覆盖率</td><td>条件覆盖数</td><td>行覆盖率</td><td>行覆盖数</td></tr></table>');
+        conf.$table.prepend($('<tr></tr>').append($('<td colspan="5">' + conf.title + ', 报表生成时间：' + (today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + (today.getDate() + 1) + ' ' + today.getHours() + ':' + today.getMinutes()) + ' 报表方式：</td>').append(conf.$mode)));
 
-		return $.each(filePool, function (i, name) {
-			var s = conf.sourceData[name], t = conf.targetData[name], deltaC, deltaL;
-			if (!s) {
-				s = t;
-			} else if (!t) {
-				t = s;
-			} //如果文件只有其中一个版本有
-			if (s.conditionalsRate === t.conditionalsRate && s.linesRate === t.linesRate) {
-				conf.$table.append('<tr class="same" style="background:#ffffff"><td><a href="' + t.link + '" target="_blank">' + name + '</a></td><td>' + s.conditionalsRate + '%</td><td>' + s.conditionsText + '</td><td>' + s.linesRate + '%</td><td>' + s.linesText + '</td></tr>');
-			} else {
-				deltaC = (t.conditionalsRate - s.conditionalsRate).toFixed(2); //条件覆盖增减
-				deltaL = (t.linesRate - s.linesRate).toFixed(2); //行覆盖增减
-				tempColor = (deltaC < 0 || deltaL < 0) ? 'ffe3e3' : 'e3ffe3'; //决定行颜色
-				conf.$table.append('<tr class="diff1" style="background:#' + tempColor + '"><td rowspan="2"><a href="' + t.link + '" target="_blank">' + name + '</a></td><td>' + s.conditionalsRate + '%</td><td>' + s.conditionsText + '</td><td>' + s.linesRate + '%</td><td>' + s.linesText + '</td></tr>');
-				conf.$table.append('<tr class="diff2" style="background:#' + tempColor + '"><td>' + t.conditionalsRate + '% (' + ((deltaC > 0) ? '+' : '') + deltaC + '%)</td><td>' + t.conditionsText + '</td><td>' + t.linesRate + '% (' + ((deltaL > 0) ? '+' : '') + deltaL + '%)</td><td>' + t.linesText + '</td></tr>');
-			}
-		});
-	}
+        return $.each(filePool, function (i, name) {
+            var s = conf.sourceData[name], t = conf.targetData[name], deltaC, deltaL;
 
-	/**
-	 * 显示结果
-	 */
-	function showReport() {
-		var css = $('<style />');
-		css.append('#report { border: 1px solid #999; border-collapse: collapse; }');
-		css.append('#report td { padding: 2px 6px; border: 1px solid #bbb; font: 13px/13px verdana; }');
-		css.append('#report tr.diff1 td { border-top: 2px solid #000; }');
-		css.append('#report tr.diff2 td, #report tr.diff1 td:first-child { border-bottom: 2px solid #000; }');
-		css.append('#report .change, #report a { cursor: pointer; color: navy; text-decoration: blink;}');
-		$('body').empty().append(conf.$table);
-		$('script').add('link').remove();
-		$('head').append(css);
-		conf.$mode.on('click', function () {
-			conf.$table.css('border-collapse', 'separate');
-			$('.same', conf.$table).toggle();
-			conf.$table.css('border-collapse', 'collapse');
-			conf.$mode.text(conf.mode = (conf.mode === '显示全部') ? '只显示差异' : '显示全部');
-		});
-	}
+            //如果文件只有其中一个版本有
+            if (!s) {
+                s = {conditionalsRate: 0, linesRate: 0, conditionsText: '', linesText: ''};
+            } else if (!t) {
+                t = {conditionalsRate: 0, linesRate: 0, conditionsText: '', linesText: ''};
+            }
 
-	/**
-	 * 检查是不是两个数字
-	 * @returns {boolean}
-	 */
-	function checkNumber() {
-		return /^\d+$/.test(conf.$sourceVer.val()) && /^\d+$/.test(conf.$targetVer.val());
-	}
+            if (s.conditionalsRate === t.conditionalsRate && s.linesRate === t.linesRate) {
+                conf.$table.append('<tr class="same" style="background:#ffffff"><td><a href="' + t.link + '" target="_blank">' + name + '</a></td><td>' + s.conditionalsRate + '%</td><td>' + s.conditionsText + '</td><td>' + s.linesRate + '%</td><td>' + s.linesText + '</td></tr>');
+            } else {
+                deltaC = (t.conditionalsRate - s.conditionalsRate).toFixed(2); //条件覆盖增减
+                deltaL = (t.linesRate - s.linesRate).toFixed(2); //行覆盖增减
+                tempColor = (s.linesText === '' || t.linesText === '') ? 'ffc3c3' : ((deltaC < 0 || deltaL < 0) ? 'ffe3e3' : 'e3ffe3'); //决定行颜色
+                conf.$table.append('<tr class="diff1" style="background:#' + tempColor + '"><td rowspan="2"><a href="' + t.link + '" target="_blank">' + name + '</a></td><td>' + s.conditionalsRate + '%</td><td>' + s.conditionsText + '</td><td>' + s.linesRate + '%</td><td>' + s.linesText + '</td></tr>');
+                conf.$table.append('<tr class="diff2" style="background:#' + tempColor + '"><td>' + t.conditionalsRate + '% (' + ((deltaC > 0) ? '+' : '') + deltaC + '%)</td><td>' + t.conditionsText + '</td><td>' + t.linesRate + '% (' + ((deltaL > 0) ? '+' : '') + deltaL + '%)</td><td>' + t.linesText + '</td></tr>');
+            }
+        });
+    }
 
-	/**
-	 * 遍历URL
-	 * @param URL
-	 * @param dataPool
-	 */
-	function analyticURL(URL, dataPool) {
-		var pathPool = [URL];
-		do {
-			analyticContent(pathPool.shift(), pathPool, dataPool);
-		} while (pathPool.length);
-	}
+    /**
+     * 显示结果
+     */
+    function showReport() {
+        var css = $('<style />');
+        css.append('#report { border: 1px solid #999; border-collapse: collapse; }');
+        css.append('#report td { padding: 2px 6px; border: 1px solid #bbb; font: 13px/13px verdana; }');
+        css.append('#report tr.diff1 td { border-top: 2px solid #000; }');
+        css.append('#report tr.diff2 td, #report tr.diff1 td:first-child { border-bottom: 2px solid #000; }');
+        css.append('#report .change, #report a { cursor: pointer; color: navy; text-decoration: blink;}');
+        $('body').empty().append(conf.$table);
+        $('script').add('link').remove();
+        $('head').append(css);
+        conf.$mode.on('click', function () {
+            conf.$table.css('border-collapse', 'separate');
+            $('.same', conf.$table).toggle();
+            conf.$table.css('border-collapse', 'collapse');
+            conf.$mode.text(conf.mode = (conf.mode === '显示全部') ? '只显示差异' : '显示全部');
+        });
+    }
 
-	/**
-	 * 抓回url内容
-	 * @param path
-	 * @param selector
-	 * @returns {*|HTMLElement}
-	 */
-	function getPathContent(path, selector) {
-		var dom = $(), retSel = selector || 'table.sortable>tbody';
-		$.ajax({
-			type: "GET",
-			url: path,
-			async: false,
-			dataType: "html",
-			success: function (result) {
-				dom = $(result);
-			}
-		});
-		return $(retSel, dom);
-	}
+    /**
+     * 检查是不是两个数字
+     * @returns {boolean}
+     */
+    function checkNumber() {
+        return /^\d+$/.test(conf.$sourceVer.val()) && /^\d+$/.test(conf.$targetVer.val());
+    }
 
-	/**
-	 * 分析内容
-	 * @param path
-	 * @param pathPool
-	 * @param dataPool
-	 */
-	function analyticContent(path, pathPool, dataPool) {
-		$('>tr', getPathContent(path)).each(function () {
-			var line = $(this).find('>td'), link = line.eq(0).find('a');
-			if (link.length) {
-				if (link.text().match(/\.js$/)) { //如果内容是一个文件
-					var conditionals = line.eq(2),
-						lines = line.eq(3),
-						rateC = +conditionals.attr('data'),
-						rateL = +lines.attr('data');
-					dataPool[link.text()] = {
-						link: path + link.attr('href'),
-						conditionalsRate: (rateC <= 100) ? rateC : 0,
-						linesRate: (rateL <= 100) ? rateL : 0,
-						conditionsText: conditionals.find('span.text').text(),
-						linesText: lines.find('span.text').text()
-					};
-				} else { //如果内容是一个路径
-					pathPool.push(path + link.attr('href'));
-				}
-			}
-		});
-	}
+    /**
+     * 遍历URL
+     * @param URL
+     * @param dataPool
+     */
+    function analyticURL(URL, dataPool) {
+        var pathPool = [URL];
+        do {
+            analyticContent(pathPool.shift(), pathPool, dataPool);
+        } while (pathPool.length);
+    }
+
+    /**
+     * 抓回url内容
+     * @param path
+     * @param selector
+     * @returns {*|HTMLElement}
+     */
+    function getPathContent(path, selector) {
+        var dom = $(), retSel = selector || 'table.sortable>tbody';
+        $.ajax({
+            type: "GET",
+            url: path,
+            async: false,
+            dataType: "html",
+            success: function (result) {
+                dom = $(result);
+            }
+        });
+        return $(retSel, dom);
+    }
+
+    /**
+     * 分析内容
+     * @param path
+     * @param pathPool
+     * @param dataPool
+     */
+    function analyticContent(path, pathPool, dataPool) {
+        $('>tr', getPathContent(path)).each(function () {
+            var line = $(this).find('>td'), link = line.eq(0).find('a');
+            if (link.length) {
+                if (link.text().match(/\.js$/)) { //如果内容是一个文件
+                    var lines = line.eq(2),
+                        conditionals = line.eq(3),
+                        rateC = +conditionals.attr('data'),
+                        rateL = +lines.attr('data');
+                    dataPool[link.text()] = {
+                        link: path + link.attr('href'),
+                        conditionalsRate: (rateC <= 100) ? rateC : 0,
+                        linesRate: (rateL <= 100) ? rateL : 0,
+                        conditionsText: conditionals.find('span.text').text(),
+                        linesText: lines.find('span.text').text()
+                    };
+                } else { //如果内容是一个路径
+                    pathPool.push(path + link.attr('href'));
+                }
+            }
+        });
+    }
 }(jQuery));
