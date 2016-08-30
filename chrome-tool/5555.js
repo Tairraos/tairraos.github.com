9611e87959e69a7afe43a3e201bfc62416748939
Xiaole Tao <xiaole.tao@gmail.com>
2016-08-30 10:21:28 +0800

    try jekyll

--- chrome-tool/5555.js
+++ chrome-tool/5555.js
@@ -1,3 +1,3 @@
 'use strict';
 var url = location.href;
-location.href = url.replace(/:55555/, '').replace(/_ijt=[a-z0-9]+/, '').replace(/\?$/, '');
+location.href = url.replace(/:55555/, '/Workspace').replace(/_ijt=[a-z0-9]+/, '').replace(/\?$/, '');
