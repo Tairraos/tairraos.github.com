f739d0623d3b6b3c5d3108c6d7012a08420ea388
Xiaole Tao <xiaole.tao@gmail.com>
2020-03-01 16:01:17 +0800

    update

--- weidai.v1.2/exp.src.js
+++ weidai.v1.2/exp.src.js
@@ -1,10 +1,12 @@
-let table = [],
-    $process = document.getElementById("app");
+"use strict";
 
+var table = [],
+    $process = document.getElementById("app"),
+    per = 10;
 table.push("<table id='list'>");
 table.push("<tr><td>微贷待还散标数据</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
 table.push("<tr><td>By 小乐</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
-table.push("<tr><td>时间戳：" + (+new Date) + "</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
+table.push("<tr><td>时间戳：" + +new Date() + "</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
 table.push([
     "<tr><td>项目名称</td>",
     "<td>日期</td>",
@@ -18,29 +20,34 @@ table.push([
 ].join(""));
 
 function getData(index) {
-    fetch("https://frontpc.weidai.com.cn/api/user/investor/returnMoney/getUserReceiveList?_api=returnMoney.getUserReceiveList&_mock=false&_stamp=" + (+
-        new Date), {
-        "credentials": "include",
-        "headers": {
-            "accept": "application/json, text/json",
-            "content-type": "application/x-www-form-urlencoded",
-        },
-        "referrer": "https://www.weidai.com.cn/home/myAccount/receivablesDetail.html",
-        "referrerPolicy": "no-referrer-when-downgrade",
-        "body": "goodsType=BIDDING&holdStatus=0&startTime=&endTime=&rows=40&page=" + index,
-        "method": "POST",
-        "mode": "cors"
-    }).then(response => response.text()).then((jsonText) => {
-        let json = JSON.parse(jsonText);
+    fetch("https://frontpc.weidai.com.cn/api/user/investor/returnMoney/getUserReceiveList?_api=returnMoney.getUserReceiveList&_mock=false&_stamp=" + +
+        new Date(), {
+            "credentials": "include",
+            "headers": {
+                "accept": "application/json, text/json",
+                "content-type": "application/x-www-form-urlencoded"
+            },
+            "referrer": "https://www.weidai.com.cn/home/myAccount/receivablesDetail.html",
+            "referrerPolicy": "no-referrer-when-downgrade",
+            "body": "goodsType=BIDDING&holdStatus=0&startTime=&endTime=&rows=" + per + "&page=" + index,
+            "method": "POST",
+            "mode": "cors"
+        }).then(function(response) {
+        return response.text();
+    }).then(function(jsonText) {
+        var json = JSON.parse(jsonText);
         return {
             count: json.data.count,
             data: json.data.data,
-            index: json.data.pageIndex,
-        }
-    }).then(resp => {
-        $process.innerHTML = Math.floor(resp.index * 40 / resp.count * 100) + "%";
-        resp.data.forEach(item => pushLine(item));
-        if (resp.index < resp.count / 40) {
+            index: json.data.pageIndex
+        };
+    }).then(function(resp) {
+        $process.innerHTML = Math.floor(resp.index * per / resp.count * 100) + "%";
+        resp.data.forEach(function(item) {
+            return pushLine(item);
+        });
+
+        if (resp.index < resp.count / per) {
             getData(resp.index + 1);
         } else {
             resolveFetch();
