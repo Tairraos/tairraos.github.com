544f23f9899fa647a2a0d03016986c643fb60cf6
Xiaole Tao <xiaole.tao@gmail.com>
2020-03-06 17:42:46 +0800

    update

--- weidai/expByXiaole.js
+++ weidai/expByXiaole.js
@@ -1,8 +1,93 @@
-"use strict";var table=[],$process=document.getElementById("app"),per=50,curIndex=1;table.push("<table id='list'>");table.push("<tr><td>微贷待还散标数据</td><td></td><td></td><td></td><td></td><td></td></tr>");table.push("<tr><td>By 小乐</td><td></td><td></td><td></td><td></td><td></td></tr>");table.push("<tr><td>时间戳："+ +new Date()+"</td><td></td><td></td><td></td><td></td><td></td></tr>");table.push(["<tr><td>项目名称</td>","<td>日期</td>","<td>预期总收(元)</td>","<td>本金(元)</td>","<td>预期利息(元)</td>","<td>偿还期数</td>","</tr>"].join(""));function getData(a){fetch("https://frontpc.weidai.com.cn/api/user/investor/returnMoney/getUserReceiveList?_api=returnMoney.getUserReceiveList&_mock=false&_stamp="+ +new Date(),{"credentials":"include","headers":{"accept":"application/json, text/json","accept-language":"zh-CN,zh;q=0.9,en;q=0.8,und;q=0.7,zh-TW;q=0.6","cache-control":"no-cache","content-type":"application/x-www-form-urlencoded","pragma":"no-cache","sec-fetch-dest":"empty","sec-fetch-mode":"cors","sec-fetch-site":"same-site"},"referrer":"https://www.weidai.com.cn/home/myAccount/receivablesDetail.html","referrerPolicy":"no-referrer-when-downgrade","body":"goodsType=BIDDING&holdStatus=0&startTime=&endTime=&rows="+per+"&page="+a,"method":"POST","mode":"cors"}).then(function(b){return b.text()}).then(function(c){var b=JSON.parse(c);return{count:b.data.count,data:b.data.data,index:b.data.pageIndex}}).then(function(b){$process.innerHTML=Math.floor(b.index*per/b.count*100)+"%";b.data.forEach(function(c){return pushLine(c)});curIndex++;if(curIndex<b.count/per){getData(curIndex+1)}else{resolveFetch()}}).catch(function(){window.setTimeout(function(){getData(curIndex)},2000)})}function resolveFetch(){table.push("</table>");document.write(table.join("\n"))}function pushLine(a){table.push(["<tr>","<td>"+a.goodsName+"</td>","<td>D"+a.recoverTime+"</td>","<td>"+a.recoverTotalAmount+"</td>","<td>"+a.recoverPrincipal+"</td>","<td>"+a.recoverInterest+"</td>","<td>第"+a.period+"</td>","</tr>"].join(""))}getData(curIndex);
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?ebaec9bcd774c0b7efc430d35a2922cd";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
+"use strict";
+
+var table = [],
+    $process = document.getElementById("app"),
+    per = 40,
+    curIndex = 1;
+
+table.push("<table id='list'>");
+table.push("<tr><td>微贷待还散标数据</td><td></td><td></td><td></td><td></td><td></td></tr>");
+table.push("<tr><td>By 小乐</td><td></td><td></td><td></td><td></td><td></td></tr>");
+table.push("<tr><td>时间戳：" + +new Date() + "</td><td></td><td></td><td></td><td></td><td></td></tr>");
+table.push([
+    "<tr><td>项目名称</td>",
+    "<td>日期</td>",
+    "<td>预期总收(元)</td>",
+    "<td>本金(元)</td>",
+    "<td>预期利息(元)</td>",
+    "<td>偿还期数</td>",
+    "</tr>"
+].join(""));
+
+
+
+function getData(index) {
+    fetch("https://frontpc.weidai.com.cn/api/user/investor/returnMoney/getUserReceiveList?_api=returnMoney.getUserReceiveList&_mock=false&_stamp=" + +
+        new Date(), {
+            "credentials": "include",
+            "headers": {
+                "accept": "application/json, text/json",
+                "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,und;q=0.7,zh-TW;q=0.6",
+                "cache-control": "no-cache",
+                "content-type": "application/x-www-form-urlencoded",
+                "pragma": "no-cache",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-site"
+            },
+            "referrer": "https://www.weidai.com.cn/home/myAccount/receivablesDetail.html",
+            "referrerPolicy": "no-referrer-when-downgrade",
+            "body": "goodsType=BIDDING&holdStatus=0&startTime=&endTime=&rows=" + per + "&page=" + index,
+            "method": "POST",
+            "mode": "cors"
+        }).then(function(response) {
+        return response.text();
+    }).then(function(jsonText) {
+        var json = JSON.parse(jsonText);
+        var resp = {
+            available: json.data.available,
+            count: json.data.count,
+            data: json.data.data,
+            index: json.data.pageIndex
+        };
+        $process.innerHTML = Math.floor(resp.index * per / resp.count * 100) + "%";
+
+        if (resp.available) {
+            resp.data.forEach(function(item) {
+                return pushLine(item);
+            });
+            curIndex++;
+            if (curIndex < resp.count / per) {
+                getData(curIndex + 1);
+            } else {
+                resolveFetch();
+            }
+        } else {
+            getData(curIndex);
+        }
+    }).catch(function() {
+        window.setTimeout(function() {
+            getData(curIndex);
+        }, 500);
+    });
+}
+
+function resolveFetch() {
+    table.push("</table>");
+    document.write(table.join("\n"));
+}
+
+function pushLine(data) {
+    table.push([
+        "<tr>",
+        "<td>" + data.goodsName + "</td>",
+        "<td>D" + data.recoverTime + "</td>",
+        "<td>" + data.recoverTotalAmount + "</td>",
+        "<td>" + data.recoverPrincipal + "</td>",
+        "<td>" + data.recoverInterest + "</td>",
+        "<td>第" + data.period + "</td>",
+        "</tr>"
+    ].join(""));
+}
+
+getData(curIndex);
